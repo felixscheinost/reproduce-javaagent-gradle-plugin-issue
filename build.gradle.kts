a431a5aacaab9903410faafa052009defe7566a5
Ryan Dens <ryan.dens@pixee.ai>
2023-12-02 17:34:50 -0800

    :bug: fix build issue by managing dependency in buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   kotlin("jvm") version "1.6.21"
   // Upgrading to 0.4.1 Breaks the build
-  id("com.ryandens.javaagent-application") version "0.4.1"
+  id("com.ryandens.javaagent-application")
 }
 
 repositories {
---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.ryandens.javaagent-application:com.ryandens.javaagent-application.gradle.plugin:0.4.1")
+}
